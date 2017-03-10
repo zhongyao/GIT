51d32f560e52fc0336dd73cbf3f4b0539f60870d
zhongyao <zhongyao@alibaba-inc.com>
2017-03-10 21:01:10 +0800

    20170329修改5

--- app/src/main/java/com/hongri/git/MainActivity.java
+++ app/src/main/java/com/hongri/git/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 /**
  * develop分支修改 develop修改"1" develop修改"2"
  * develop_20170315提交 20170315修改"1" 20170315修改"2" 20170315修改"3"
- * develop_20170329修改 20170329修改"1" 20170329修改"2" 20170329修改"3" 20170329修改"4"
+ * develop_20170329修改 20170329修改"1" 20170329修改"2" 20170329修改"3" 20170329修改"4" 20170329修改"5"
  */
 public class MainActivity extends AppCompatActivity {
 
