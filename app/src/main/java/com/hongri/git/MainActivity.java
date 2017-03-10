e8f80d8eb688a86cc573ce0cb7ca305f3bb461df
zhongyao <zhongyao@alibaba-inc.com>
2017-03-10 10:07:15 +0800

    develop分支修改

--- app/src/main/java/com/hongri/git/MainActivity.java
+++ app/src/main/java/com/hongri/git/MainActivity.java
@@ -3,6 +3,9 @@ package com.hongri.git;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * develop分支修改
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
