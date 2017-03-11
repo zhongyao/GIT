c70ea25fa1c21e8017f299d22feac049dab8bfcf
zhongyao <zhongyao@alibaba-inc.com>
2017-03-11 11:34:33 +0800

    Revert "error commit"
    This reverts commit 8f7749f925b0524af87081ba20536bf321719c7c.

--- app/src/main/java/com/hongri/git/MainActivity.java
+++ app/src/main/java/com/hongri/git/MainActivity.java
@@ -9,8 +9,6 @@ import android.os.Bundle;
  * develop_20170329修改 20170329修改"1" 20170329修改"2" 20170329修改"3" 20170329修改"4" 20170329修改"5" 20170329修改"6"
  * develop_20170415修改 20170415修改"1" 20170415修改"2"
  * develop_20170429修改 20170429修改"1" 20170429修改"2" 20170429修改"3" 20170429修改"4"
- *
- * error commit
  */
 public class MainActivity extends AppCompatActivity {
 
